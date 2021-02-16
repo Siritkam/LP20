39f1d239daafa4aa464cd3ffaa6ae4eea466de5e
Siritkam <danidoom@yandex.ru>
2021-02-16 20:41:22 +0300

    строки

--- lesson2/Strings.py
+++ lesson2/Strings.py
@@ -4,14 +4,12 @@ string2 = input('Введите вторую строку: ')
 def string_chk (string1, string2):
     if string1 and string2 is type(str()):
         return(0)
-    elif:
-        string1 == string2
+    elif string1 == string2:
         return(1)
-    elif: 
-        string1 <> string2 and len(string1) > len(string2)
+    elif string1 != string2 and len(string1) > len(string2):
         return(2)
-    elif: 
-        string1 <> string2 and string2 = 'learn'
+    elif string1 != string2 and string2 == 'learn':
         return(3)
+    else: return ('Ни одно условие не выполнено')
 
-string_chk(string1, string2)
+print(string_chk(string1, string2))
