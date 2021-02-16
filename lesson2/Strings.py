e1f9cfb9daaf1fc92170f15f0e22cf56683607d0
Siritkam <danidoom@yandex.ru>
2021-02-16 17:33:58 +0300

    Первое задание

---
+++ lesson2/Strings.py
@@ -0,0 +1,13 @@
+def string_chk (string1, string2):
+    if string1 and string2 is type(str()):
+        return(0)
+    elif:
+        string1 == string2
+        return(1)
+    elif: 
+        string1 <> string2 and len(string1) > len(string2)
+        return(2)
+    elif: 
+        string1 <> string2 and string2 = 'learn'
+        return(3)
+          
